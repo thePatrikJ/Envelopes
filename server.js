1ee755ab77f58353a2f63d871027a6cbb31a776f
Patrik Jarolim <jarolimpatrik0@gmail.com>
2025-01-08 22:15:57 +0100

    Added post to create new envelopes

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const PORT = 4000;
 
 app.get('/', (req, res) => {
     res.send('HELLO');
+})
+    const envelopes = [];
+    let totalBudget = 0;
+    let nextId = 1
+
+app.post('/newEnv', (req, res) => {
+    let newEnv = req.body;
+    if (newEnv && newEnv.amount && newEnv.name) {
+        newEnv.id = nextId++;
+        envelopes.push(newEnv);
+        totalBudget += newEnv.amount;
+        res.status(201).send(`Succes! ${totalBudget}, ${envelopes}`);
+        console.log('succes');
+    }
+    else {
+        res.sendStatus(400);
+    }
 })
 
 app.listen(PORT, () => {
