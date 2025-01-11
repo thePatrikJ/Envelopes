e18a00659ccb254c9177e0ca201becf2d362cf92
Patrik Jarolim <jarolimpatrik0@gmail.com>
2025-01-11 10:05:20 +0100

    added post req to trandfeer amount from an id to a different id

--- server.js
+++ server.js
@@ -77,6 +77,23 @@ app.delete('/envelopes/:id', (req, res) => {
     }
 })
 
+app.post("/envelopes/transfer", (req, res) => {
+    const {fromId, toId} = req.query;
+    const {amount} = req.body;
+    const fromEnv = envelopes.find(env => {env.id === Number(fromId)})
+    const toEnv = envelopes.find(env => {env.id === Number(toId)})
+    if (!fromEnv || !toEnv) {
+        res.sendStatus(404);
+    }
+    if (fromEnv.amount < amount) {
+        res.send('Insufficient funds!!! in the source envelope!')
+    }
+    //transfer
+    fromEnv.amount -= amount;
+    toEnv.amount += amount;
+    res.status(200).send('The transfer was Succesful!');
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
 })
